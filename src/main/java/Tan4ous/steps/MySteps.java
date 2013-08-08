1a36a013ed302aecad299f113b8a29c6b8333ad6
Drondina Tatyana <irienals@gmail.com>
2013-08-08 21:43:19 +0400

    not work

--- src/main/java/Tan4ous/steps/MySteps.java
+++ src/main/java/Tan4ous/steps/MySteps.java
@@ -1,10 +1,38 @@
 package Tan4ous.steps;
 
-import org.jbehave.core.annotations.Given;
-import org.jbehave.core.annotations.Pending;
-import org.jbehave.core.annotations.Then;
-import org.jbehave.core.annotations.When;
+import Tan4ous.Point;
+import org.jbehave.core.annotations.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class MySteps {
-    
+
+    private String status;
+    private Point point;
+    int sum = 0, b1;
+
+    @Given("b is <b>")
+    public void givenAStockOfb(@Named("b") int b) {
+        b1=b;
+
+    }
+
+    @When("sum of a or b")
+    @Alias("sum of a or b")
+    @Composite(steps = {"Given <user> is logged in", "Given bonus is <bonus>"})
+    public void aCompositeStep(@Named("user") String user, @Named("bonus") int bonus) {
+        sum = bonus + b1;
+    }
+    public void whenTheStockIsTradedAtsum(@Named("sum") Double sum) {
+        status = point.checkOutSum(sum);
+    }
+
+    @Then("sum is <result>")
+    public void thenTheAlertStatusShouldBeresult(@Named("result") String stat) {
+        assertEquals(stat, status);
+    }
+
+
+
+
 }
